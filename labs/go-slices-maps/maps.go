a983cbeac6a73ee25e1c0515ccf641baca740a75
Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>
2020-03-10 21:44:54 -0600

    Solved go-slices-maps
    Signed-off-by: Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	ret := make(map[string]int)
+	for i:=0; i<len(words); i++{
+		ret[words[i]]++;
+	}
+	return ret
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,22 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	matrix := make([][]uint8, dy)
+	for i:=range matrix {
+		matrix[i] = make([]uint8,dx)
+	}
+	
+	for x:=0; x<dy; x++{
+		for y:=0; y<dx; y++{
+			matrix[x][y] = uint8(x+y)
+		}
+	}
+	
+	return matrix
+}
+
 func main() {
+	pic.Show(Pic)
 }
