6a8dce1a8a1dc6553a13e7334623f87ca6ba0418
Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>
2020-03-23 19:33:44 -0600

    Solved first-partial
    Signed-off-by: Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>

--- challenges/first-partial/dir-scan.go
+++ challenges/first-partial/dir-scan.go
@@ -3,11 +3,51 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 // scanDir stands for the directory scanning implementation
+
+var dirs,symlins,devs,socs,oths int = 0,0,0,0,0
+
 func scanDir(dir string) error {
+
+	filepath.Walk(dir, classification)
+	fmt.Println(dirs)
+	fmt.Println(symlins)
+	fmt.Println(devs)
+	fmt.Println(socs)
+	fmt.Println(oths)
+
+	fmt.Println("Directory Scanner Tool")
+	fmt.Println("+-----------------------+-----------+")
+	fmt.Println("| Path\t\t\t| ",dir)
+	fmt.Println("+-----------------------+-----------+")
+	fmt.Println("| Directories\t\t| ",dirs)
+	fmt.Println("| Symbolic Links\t| ",symlins)
+	fmt.Println("| Devices\t\t| ",devs)
+	fmt.Println("| Sockets\t\t| ",socs)
+	fmt.Println("| Other files\t\t| ",oths)
+	fmt.Println("+-----------------------+-----------+")
+
 	return nil
+}
+
+func classification(path string, info os.FileInfo, err error) error {
+	mode := os.FileMode.String(info.Mode())[0:1]
+	switch mode {
+		case "d":
+			dirs++
+		case "L":
+			symlins++
+		case "D":
+			devs++
+		case "S":
+			socs++
+		default:
+			oths++
+	}
+	return nil;
 }
 
 func main() {
