72f390b83c1d33f02aede396757b7d1a34ab158c
Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>
2020-03-25 20:28:04 -0600

    Solved mycat lab
    Signed-off-by: Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>

--- labs/mycat/cat2.c
+++ labs/mycat/cat2.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /* filecopy:  copy file ifp to file ofp */
-void filecopy(FILE *ifp, FILE *ofp)
+void filecopy(int ifpd, int ofpd)
 {
-    int c;
 
-    while ((c = getc(ifp)) != EOF)
-        putc(c, ofp);
+    	int end = 1;
+	char readC[1];
+
+	end = read(ifpd,readC,1);
+	//printf("%d\n",ifpd);
+
+	while (end > 0){
+		//end = read(ifpd,readC,1);
+		//printf("%d\n",end);
+		write(ofpd,readC,1);
+		end = read(ifpd,readC,1);
+
+	}
 
 }
 
 /* cat:  concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
-    FILE *fp;
-    void filecopy(FILE *, FILE *);
+    int fp;
+    void filecopy(int, int);
     char *prog = argv[0];   /* program name for errors */
 
-    if (argc == 1)  /* no args; copy standard input */
-        filecopy(stdin, stdout);
-    else
-        while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL) {
-                fprintf(stderr, "%s: can′t open %s\n",
-			prog, *argv);
+    if (argc == 1){  /* no args; copy standard input */
+        filecopy(0, 1);
+    }else{
+        while (--argc > 0){
+		//printf("%s\n",argv[v]);
+		int fp = open(*++argv,O_RDONLY);
+            if (fp < 0) {
+                fprintf(stderr, "%s: can′t open %s\n", prog, *argv);
                 return 1;
             } else {
-                filecopy(fp, stdout);
-                fclose(fp);
+		//printf("%d\n",fp);
+                filecopy(fp, 1);
+                close(fp);
             }
+
+		}
+	}
 
     if (ferror(stdout)) {
         fprintf(stderr, "%s: error writing stdout\n", prog);
