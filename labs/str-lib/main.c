b9506e8b33623de80ab60a26a5ca375069080edc
Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>
2020-03-25 20:45:55 -0600

    Solved str-lib lab
    Signed-off-by: Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int main(int argc, char **argv) {
+
+	int mystrlen(char *str);
+	char *mystradd(char *origin, char *addition);
+	int mystrfind(char *origin, char *substr);
+
+	if(argc < 4){
+		printf("Usage: ./main initialString additionalString substring\n");
+		return 1;
+	} else{
+		int len = mystrlen(argv[1]);
+		char *res = mystradd(argv[1],argv[2]);
+		int sub = mystrfind(res,argv[3]);
+
+		printf("Initial Length\t\t: %d\n",len);
+		printf("New String\t\t: %s\n",res);
+		if(mystrfind(res,argv[3]) == 0){
+			printf("SubString was found\t: yes\n");
+		} else{
+			printf("SubString was found\t: no\n");
+		}
+	}
+
+	return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,56 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+	char a[1];
+	int v = 0;
+	a[0] = str[0];
+	while(a[0] != '\0'){
+		v++;
+		a[0] = str[v];
+	}
+	return v;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    	char *a;
+	a = malloc(mystrlen(origin)+mystrlen(addition)+1);
+	char b;
+	int v = 0;
+	int w = 0;
+	b = origin[0];
+	while(b != '\0'){
+		a[v]=b;
+		v++;
+		b = origin[v];
+	}
+	b = addition[0];
+	while(b != '\0'){
+		a[v] = b;
+		w++;
+		v++;
+		b = addition[w];
+	}
+	a[mystrlen(origin)] = *addition;
+
+	return a;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+	int i;
+	int j = 0;
+	int ret = 1;
+	for(i=0;i<=mystrlen(origin);i++){
+		char a = origin[i];
+		char b = substr[j];
+		if(b=='\0'){
+			ret = 0;
+			break;
+		} else if(a == b){
+			j++;
+		} else{
+			j = 0;
+		}
+	}
+    	return ret;
 }
