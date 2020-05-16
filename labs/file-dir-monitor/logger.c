776c6e7e2f2f9529c9825b66f55acc975ae3f141
Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>
2020-05-15 22:23:42 -0500

    Solved file-dir-monitor lab
    Signed-off-by: Rogelio Alberto Felix Gamboa <A01634386@itesm.mx>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,94 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <signal.h>
 #include "logger.h"
+#include <string.h>
+#include <syslog.h>
+
+#define RED "\x1b[1;31m"
+#define GREEN "\x1b[1;32m"
+#define YELLOW "\x1b[1;33m"
+#define MAGENTA "\x1b[1;35m"
+#define RESET "\x1b[0m"
+
+int stdoutLog = 1;
+
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
 
 int initLogger(char *logType) {
+	if(strcmp(logType,"stdout") * strcmp(logType,"") == 0){
+		stdoutLog = 1;
+	} else if(strcmp(logType,"syslog") == 0){
+		stdoutLog = 0;
+	} else{
+		printf("Error: logType must either be stdout, syslog or empty string. LogType will remain unchanged.\n");
+		return 1;
+	}
     printf("Initializing Logger on: %s\n", logType);
     return 0;
 }
 
-int infof(const char *format, ...) {
-    return 0;
+int infof(const char *format, ...){
+	va_list arg;
+	va_start(arg,format);
+	printf(GREEN);
+	int ret = 0;
+	if(stdoutLog){
+		ret = vprintf(format, arg);
+	} else{
+		vsyslog(LOG_INFO, format, arg);
+	}
+	va_end(arg);
+	printf(RESET);
+	return ret;
 }
 
-int warnf(const char *format, ...) {
-    return 0;
+int warnf(const char *format, ...){
+	va_list arg;
+	va_start(arg,format);
+	printf(YELLOW);
+	int ret = 0;
+	if(stdoutLog){
+		ret = vprintf(format,arg);
+	} else{
+		vsyslog(LOG_WARNING,format,arg);
+	}
+	va_end(arg);
+	printf(RESET);
+	return ret;
 }
 
-int errorf(const char *format, ...) {
-    return 0;
-}
-int panicf(const char *format, ...) {
-    return 0;
+int errorf(const char *format, ...){
+	va_list arg;
+	va_start(arg,format);
+	printf(RED);
+	int ret = 0;
+	if(stdoutLog){
+		ret = vprintf(format,arg);
+	} else{
+		vsyslog(LOG_ERR,format,arg);
+	}
+	va_end(arg);
+	printf(RESET);
+	return ret;
 }
 
+int panicf(const char *format, ...){
+	va_list arg;
+	va_start(arg,format);
+	printf(MAGENTA);
+	int ret = 0;
+	if(stdoutLog){
+		ret = vprintf(format,arg);
+	} else{
+		vsyslog(LOG_EMERG,format,arg);
+	}
+	va_end(arg);
+	printf(RESET);
+	fflush(stdout);
+	return raise(SIGABRT);
+}
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,101 @@
-#include <stdio.h>
+#define _XOPEN_SOURCE 500
+#include <stdlib.h>
+#include <unistd.h>
+#include <ftw.h>
 #include "logger.h"
+#include <sys/inotify.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+#define BUFFER_LEN sizeof(struct inotify_event) * 1024
+
+int inotifyFd, wd;
+char *root;
+
+int add_subd(const char *filename, const struct stat *statptr, int fileflags, struct FTW *pfwt){
+	if(fileflags == FTW_D){
+		wd = inotify_add_watch(inotifyFd, filename, IN_ALL_EVENTS);
+		if(wd == -1){
+			errorf("Could not add to inotify instance\n");
+			return 2;
+		}
+		infof("%s was added to inotify instance\n",filename);
+	}
+
+	return 0;
+
+}
+
+int init_root(){
+	infof("\n\n");
+	inotifyFd = inotify_init();
+	if(inotifyFd == -1){
+		errorf("Could not initialize inotify instance\n");
+		return 1;
+	}
+
+	wd = inotify_add_watch(inotifyFd, root, IN_MODIFY | IN_CREATE | IN_DELETE);
+	if(wd == -1){
+		errorf("Could not add to inotify instance\n");
+		return 2;
+	}
+
+	nftw(root, add_subd, 5, 0);
+
+	infof("\n\n");
+}
+
+int displayInotifyEvent(struct inotify_event *i){
+	if(i->mask & IN_MODIFY)		infof("%s was modified\n", i->name);
+	if(i->mask & IN_CREATE){
+		close(inotifyFd);
+		init_root();
+		infof("%s was created\n",i->name);
+		infof("Inotify instance was updated\n");
+	}
+	if(i->mask & IN_DELETE){
+		close(inotifyFd);
+		init_root();
+		infof("%s was deleted\n",i->name);
+		infof("Inotify instance was updated\n");
+	}
+}
+
+int main(int argc, char** argv){
+
+	initLogger("stdout");
+
+	char* buf = (char*)malloc(BUFFER_LEN);
+	ssize_t numRead;
+	char *p;
+	struct inotify_event *event;
+
+	if(argc < 2){
+		errorf("Error. Correct usage: ./monitor <path>");
+		return -1;
+	}
+
+	root = argv[1];
+
+	init_root();
+
+	for(;;){
+		numRead = read(inotifyFd, buf, BUFFER_LEN);
+		if(numRead == 0){
+			panicf("Could not read from inotify instance\n");
+			return 3;
+		}
+		if(numRead == 1){
+			errorf("Could not read from inotify instance\n");
+			return 4;
+		}
+
+		for(p = buf; p < buf + numRead;){
+			event = (struct inotify_event *)p;
+			displayInotifyEvent(event);
+
+			p += sizeof(struct inotify_event) + event->len;
+		}
+
+	}
+
+	return 0;
 }
